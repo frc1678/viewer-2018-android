2c9ead60a13f34b1e2d0e24a58a53ae5874dde65
Sam <meetsammy1007@gmail.com>
2017-02-04 16:00:01 -0800

    scout viewer functionality all works

--- androidviewertemplates/src/main/java/com/example/evan/androidviewertemplates/firebase_classes/TeamInMatchData.java
+++ androidviewertemplates/src/main/java/com/example/evan/androidviewertemplates/firebase_classes/TeamInMatchData.java
@@ -11,7 +11,7 @@ import java.util.Map;
 public class TeamInMatchData extends com.example.evan.androidviewertools.firebase_classes.TeamInMatchData {
     public CalculatedTeamInMatchData calculatedData;
 
-    public ArrayList<String> scoutName;
+    public String scoutName;
     public Boolean didLiftoff;
     public Boolean didStartDisabled;
     public Boolean didBecomeIncapacitated;
