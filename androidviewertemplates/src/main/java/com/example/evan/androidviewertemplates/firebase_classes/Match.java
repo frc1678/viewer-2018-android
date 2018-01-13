f886d42f599073ef0d174f46c5e1a178aceb64c2
thondascully <32531217+thondascully@users.noreply.github.com>
2018-01-12 21:15:18 -0800

    Ready for Kate (#10)
    * add CalculatedMatchData and CalculatedTeamData
    
    Added all current variables for CalculatedMatchData and
    CalculatedTeamData
    
    * Add TeamTemplate CalculatedTeamInMatchData + 1
    
    Added TeamTemplate, CalculatedTeamInMatchData and Competition
    
    * Update MainActivity.java
    
    * Update ViewerApplication.java
    
    * Update MainActivity.java
    
    * Update TeamTemplate Map + AL Syntax
    
    * Update TeamTemplate ArrayList + Map Syntax
    
    * Add Match.java class in firebase_classes
    
    * pre-deletion (teo personal)
    
    * Delete Kate's Classes and ready for Kate
    
    * Ready for Kate 2.0
    
    * Ready for Kate 3.0
    
    * Update Match.java
    
    * Update Match.java

--- androidviewertemplates/src/main/java/com/example/evan/androidviewertemplates/firebase_classes/Match.java
+++ androidviewertemplates/src/main/java/com/example/evan/androidviewertemplates/firebase_classes/Match.java
@@ -3,11 +3,35 @@ package com.example.evan.androidviewertemplates.firebase_classes;
 import com.example.evan.androidviewertools.firebase_classes.*;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
 
 /**
- * Created by evan on 6/18/16.
+ * Created by Teo on 1/11/18.
  */
 public class Match extends com.example.evan.androidviewertools.firebase_classes.Match {
-    //public CalculatedMatchData calculatedData;
+    public CalculatedMatchData calculatedData;
+    //Make sure all variables are public
+    public Integer number;
+    public ArrayList<String> redAllianceTeamNumbers;
+    public ArrayList<String> blueAllianceTeamNumbers;
+    public Map<String, Integer> blueCubesForPowerup;
+    public Map<String, Integer> blueCubesInVaultFinal;
+    public Boolean blueDidAutoQuest;
+    public Boolean blueDidFaceBoss;
+
+
+    public Map<String, Integer> redCubesForPowerup;
+    public Map<String, Integer> redCubesInVaultFinal;
+    public Boolean redDidAutoQuest;
+    public Boolean redDidFaceBoss;
+    public Map<String, String> redSwitch;
+    public Map<String, String> blueSwitch
+    public Map<String, String> scale;
+    public Integer redScore;
+
+    public Integer blueScore;
+    public Integer foulPointsGainedRed;
+    public Integer foulPointsGainedBlue;
 
 }
