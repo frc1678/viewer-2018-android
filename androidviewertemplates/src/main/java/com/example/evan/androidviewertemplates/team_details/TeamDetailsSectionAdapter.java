afd908993e0c7a9ac407bffd3ef9c82580925373
KateUnger <32380357+KateUnger@users.noreply.github.com>
2018-02-11 13:12:32 -0800

    pitRampTimes Displayed (#44)
    * Added predictedPark
    
    * pitRampTimes Displayed!

--- androidviewertemplates/src/main/java/com/example/evan/androidviewertemplates/team_details/TeamDetailsSectionAdapter.java
+++ androidviewertemplates/src/main/java/com/example/evan/androidviewertemplates/team_details/TeamDetailsSectionAdapter.java
@@ -39,7 +39,7 @@ public class TeamDetailsSectionAdapter extends MultitypeRankingsSectionAdapter {
             {"calculatedData.avgSpeed","calculatedData.avgAgility","calculatedData.avgDefense",
                     "calculatedData.totalNumGoodDecisions","calculatedData.totalNumBadDecisions","totalSuperNotes"},
             {"pitDriveTrain","pitCanCheesecake","pitClimberType","pitSEALsNotes",
-            "pitAvailableWeight","pitProgrammingLanguage","calculatedData.pitRampTime"}
+            "pitAvailableWeight","pitProgrammingLanguage","pitRampTimes"}
 
 
     };
@@ -73,21 +73,24 @@ public class TeamDetailsSectionAdapter extends MultitypeRankingsSectionAdapter {
             "pitDriveTrain",
             "superNotes",
             "climb",
-            "totalSuperNotes"
+            "totalSuperNotes",
+            "pitRampTimes"
     };
 
     private String[] shouldDisplayAsLongText = {
             "pitNotes",
             "superNotes",
             "pitSEALsNotes",
-            "totalSuperNotes"
+            "totalSuperNotes",
+            "pitRampTimes"
 
     };
 
     private String[] shouldDisplayAsFurtherInformation = {
             "matches",
             "superNotes",
-            "totalSuperNotes"
+            "totalSuperNotes",
+
     };
 
     private String[] notClickableFields = {
@@ -100,6 +103,7 @@ public class TeamDetailsSectionAdapter extends MultitypeRankingsSectionAdapter {
             "pitOrganization",
             "pitDriveTrain",
             "pitCanCheesecake",
+            "pitRampTimes"
             //"superNotes"
     };
 
