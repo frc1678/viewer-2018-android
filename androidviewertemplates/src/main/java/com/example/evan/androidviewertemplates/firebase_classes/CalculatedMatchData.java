f555909a1d6616946ea7ee3747a3289523f22495
thondascully <32531217+thondascully@users.noreply.github.com>
2018-07-12 11:58:02 -0700

    Moderate amount of changes w/ Code cleanup (#107)
    * 6:13pm
    
    * termine
    
    * Last minute spelling change
    
    * Color change

--- androidviewertemplates/src/main/java/com/example/evan/androidviewertemplates/firebase_classes/CalculatedMatchData.java
+++ androidviewertemplates/src/main/java/com/example/evan/androidviewertemplates/firebase_classes/CalculatedMatchData.java
@@ -7,23 +7,5 @@ package com.example.evan.androidviewertemplates.firebase_classes;
 public class
 CalculatedMatchData extends Object {
     //Make sure all variables are public
-    public Float predictedBlueScore;
-    public Float predictedRedScore;
-    public Float predictedBlueRPs;
-    public Integer actualBlueRPs;
-    public Float predictedRedRPs;
-    public Integer actualRedRPs;
-    public Float predictedBlueAutoQuest;
-    public Float predictedRedAutoQuest;
-    public Float redWinChance;
-    public Float blueWinChance;
-    public Float redPredictedPark;
-    public Float bluePredictedPark;
-    public Float redLevitateProbability;
-    public Float blueLevitateProbability;
-    public Float redTeleopExchangeAbility;
-    public Float blueTeleopExchangeAbilit;
-    public Float redPredictedFaceTheBoss;
-    public Float bluePredictedFaceTheBoss;
 
 }
--- androidviewertemplates/src/main/java/com/example/evan/androidviewertemplates/firebase_classes/Match.java
+++ androidviewertemplates/src/main/java/com/example/evan/androidviewertemplates/firebase_classes/Match.java
@@ -13,23 +13,5 @@ public class Match extends com.example.evan.androidviewertools.firebase_classes.
     public CalculatedMatchData calculatedData;
     //Make sure all variables are public
 
-    public Map<String, Integer> blueCubesForPowerup;
-    public Map<String, Integer> blueCubesInVaultFinal;
-    public Boolean blueDidAutoQuest;
-    public Boolean blueDidFaceBoss;
-    public Boolean blueDidiFaceBoss; //todo Delete Later.
-
-    public Map<String, Integer> redCubesForPowerup;
-    public Map<String, Integer> redCubesInVaultFinal;
-    public Boolean redDidAutoQuest;
-    public Boolean redDidFaceBoss;
-    public Boolean redDidiFaceBoss; //todo Delete Later.
-    public Map<String, String> redSwitch;
-    public Map<String, String> blueSwitch;
-    public Map<String, String> scale;
-//.
-
-    public Integer foulPointsGainedRed;
-    public Integer foulPointsGainedBlue;
 
 }
