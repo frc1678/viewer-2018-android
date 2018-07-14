dbaafdb7d3bf82e037f6f01b267bb9287f7a3a29
Carl Csaposs <32380359+ccsaposs@users.noreply.github.com>
2018-07-14 14:59:20 -0700

    Super Code Cleanup (#110)

--- androidviewertemplates/src/main/java/com/example/evan/androidviewertemplates/drawer_fragments/super_ability/SuperAbilityAdapter.java
+++ androidviewertemplates/src/main/java/com/example/evan/androidviewertemplates/drawer_fragments/super_ability/SuperAbilityAdapter.java
@@ -13,11 +13,6 @@ public class SuperAbilityAdapter extends FragmentStatePagerAdapter {
     Context context;
     //todo
     String[] fields = {
-            "calculatedData.avgSpeed",
-            "calculatedData.avgAgility",
-            "calculatedData.avgDefense",
-            "calculatedData.totalNumGoodDecisions",
-            "calculatedData.totalNumBadDecisions"
     };
 
     public SuperAbilityAdapter(Context context, FragmentManager mgr) {
