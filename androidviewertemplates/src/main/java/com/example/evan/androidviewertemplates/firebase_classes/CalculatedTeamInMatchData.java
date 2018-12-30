67a08e54eb244cfc524150d70b16578c5e278a25
Kate Unger <kmoomie123@gmail.com>
2018-12-29 22:17:28 -0800

    fixed CalculatedTeamInMatchData

--- androidviewertemplates/src/main/java/com/example/evan/androidviewertemplates/firebase_classes/CalculatedTeamInMatchData.java
+++ androidviewertemplates/src/main/java/com/example/evan/androidviewertemplates/firebase_classes/CalculatedTeamInMatchData.java
@@ -9,50 +9,5 @@ import java.util.Map;
 
 @JsonIgnoreProperties(ignoreUnknown = false)
 public class CalculatedTeamInMatchData extends Object {
-    public Float allianceSwitchCycleTimeTele;
-    public Float allianceSwitchCycleTimeAuto;
-    public Float scaleCycleTimeTele;
-    public Float scaleCycleTimeAuto;
-    public Float opponentSwitchCycleTimeTele;
-    public Integer numAllianceSwitchAuto;
-    public Integer numAllianceSwitchTele;
-    public Integer numOpponentSwitchTele;
-    public Integer numScaleAuto;
-    public Integer numScaleTele;
-    public Integer numCubesScaleAt100s;
-    public Integer numCubesScaleAt110s;
-    public Integer avgVaultTime;
-    public Boolean didClimb;
-    public Integer numZoneOneIntakeAuto;
-    public Integer numZoneTwoIntakeAuto;
-    public Integer numZoneThreeIntakeAuto;
-    public Integer numZoneFourIntakeAuto;
-    public Integer numZoneOneIntakeTele;
-    public Integer numZoneTwoIntakeTele;
-    public Integer numZoneThreeIntakeTele;
-    public Integer numZoneFourIntakeTele;
-    public Integer numCubesPlacedAuto;
-    public Integer numCubesPlacedTele;
-    public Integer totalCubesPlaced;
-    public Boolean isDysfunctional;
-    public Float drivingAbility;
-    public Boolean didThreeExchangeInput;
-    public Integer numRobotsLifted;
-    public Float timeToOwnAllianceSwitchAuto;
-    public Float timeToOwnScaleAuto;
-    public Integer numExchangeInput;
-    public Integer numCubesDroppedAuto;
-    public Integer numCubesDroppedTele;
-    public Integer totalNumScaleFoul;
-    public Integer numSpilledCubesAuto;
-    public Integer numSpilledCubesTele;
-    public Integer rankAgility;
-    public Integer rankDefense;
-    public Integer rankSpeed;
-    public Object startingPosition;
-    public String scoutName;
-    public String superNotes;
-    public Float exchangeCycleTime;
-
-
+//make sure all data points are public
 }
